b882db2941d05c12e092bf5de5e1ecce9871e19c
Esteban Mellado <emellado@algeiba.com>
2020-06-26 15:44:17 -0300

    add automapper

---
+++ B2B_Policy_Lambda/Mapper/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using B2B_Policy_Lambda.Mapper.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace B2B_Policy_Lambda.Mapper
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddMapperServices(this IServiceCollection services)
+        {
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new PolicyProfile());
+                mc.AllowNullCollections = true;
+            });
+            var mapper = mappingConfig.CreateMapper();
+            mapper.ConfigurationProvider.CompileMappings();
+
+            services.AddSingleton(mapper);
+
+            return services;
+        }
+    }
+}
---
+++ B2B_Policy_Lambda/Mapper/Profiles/PolicyProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using B2B_Policy_Lambda.Dto.Commands.AddPolicy;
+using B2B_Policy_Lambda.Dto.Commands.RemovePolicy;
+using B2B_Policy_Lambda.Dto.Queries.GetPolicies;
+using B2B_Policy_Lambda.Models;
+
+namespace B2B_Policy_Lambda.Mapper.Profiles
+{
+    public class PolicyProfile : Profile
+    {
+        public PolicyProfile()
+        {
+            CreateMap<PolicyModel, PolicyDto>();
+            CreateMap<AddPolicyRequest, PolicyModel>();
+            CreateMap<RemovePolicyRequest, PolicyModel>();
+        }
+    }
+}
